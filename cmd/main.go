d848f82f95a44a9c4f12929d4c097bc877d1cbd1
MhmdRyhn <rayhan.cse12@gmail.com>
2020-10-25 04:04:12 +0600

    Write the lambda function in golang

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main;
+
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+
+type LambdaEvent struct {
+	Source string `json:"source"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+
+
+type LambdaResponse struct {
+	Message string `json:"message"`
+}
+
+
+func lambdaHandler(event LambdaEvent) (LambdaResponse, error) {
+	return LambdaResponse {
+		Message: fmt.Sprintf("%s is %d years old.", event.Name, event.Age),
+	}, nil
+}
+
+
+func main ()  {
+	lambda.Start(lambdaHandler)
+	// fmt.Println("running")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mhmdryhn/go-lambda
+
+go 1.15
+
+require github.com/aws/aws-lambda-go v1.19.1
